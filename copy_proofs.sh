54880e20cd73440c257ad8a26cd0b8ee0e74210b
valeriob01 <valerio@selnet.org>
2020-07-24 14:54:45 +0200

    Update copy_proofs.sh

--- copy_proofs.sh
+++ copy_proofs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-set -o xtrace
-
 if [[ $# -eq 2 ]]; then
   # Copy files to upload location.
 find $1 -name '*.proof' -exec cp -v -n '{}' $2 \;
 elif [[ $# -lt 2 ]]; then
-  echo "Must specify original and destination directory."; exit;
+  echo "Must specify original and destination directories."; exit;
+elif [[ $# -gt 2 ]]; then
+  echo "Too much parameters. Must specify original and destination directories only."; exit;
 fi
