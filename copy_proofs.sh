3a29faa1e0788e3c8adf124d648c8f0a6df4d018
valeriob01 <valerio@selnet.org>
2020-07-24 14:34:45 +0200

    Update copy_proofs.sh
    test new version.

--- copy_proofs.sh
+++ copy_proofs.sh
@@ -2,9 +2,12 @@
 
 set -o xtrace
 
+
+ [[ -z "$2" ]] && echo "Must specify destination directory." || [[ -z "$1" ]] && echo "Must specify original directory."; exit;
+
 # Copy files to upload location.
 find $1 -name '*.proof' -exec cp -v -n '{}' $2 \;
 
-# Also copy to a backup location.
- [[ ! -z "$3" ]] && find $1 -name '*.proof' -exec cp -v -n '{}' $3 \; || echo "No backup location specified, proof files still copied to upload location."
+# Not nessary anymore ## Also copy to a backup location.
+# [[ ! -z "$3" ]] && find $1 -name '*.proof' -exec cp -v -n '{}' $3 \; || echo "No backup location specified, proof files still copied to upload location."
  
