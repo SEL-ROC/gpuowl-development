666614ac6103e387a070db0913398aee1e5ab6c2
valeriob01 <valerio@selnet.org>
2020-07-24 14:55:50 +0200

    Update copy_proofs.sh

--- copy_proofs.sh
+++ copy_proofs.sh
@@ -4,7 +4,7 @@ if [[ $# -eq 2 ]]; then
   # Copy files to upload location.
 find $1 -name '*.proof' -exec cp -v -n '{}' $2 \;
 elif [[ $# -lt 2 ]]; then
-  echo "Must specify original and destination directories."; exit;
+  echo "Too few arguments. Must specify original and destination directories."; exit;
 elif [[ $# -gt 2 ]]; then
-  echo "Too much parameters. Must specify original and destination directories only."; exit;
+  echo "Too much arguments. Must specify original and destination directories only."; exit;
 fi
