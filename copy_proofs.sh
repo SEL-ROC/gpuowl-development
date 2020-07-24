13c67769a22f7e5750ece5913fca42aff2da0f66
valeriob01 <valerio@selnet.org>
2020-07-24 15:00:32 +0200

    Update copy_proofs.sh

--- copy_proofs.sh
+++ copy_proofs.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ $# -eq 2 ]]; then
+  if [[ "$1" -eq "$2" ]] then
+    echo "Cannot copy to the same directory."; exit;
+  fi
   # Copy files to upload location.
-find $1 -name '*.proof' -exec cp -v -n '{}' $2 \;
+  find $1 -name '*.proof' -exec cp -v -n '{}' $2 \;
 elif [[ $# -lt 2 ]]; then
   echo "Too few arguments. Must specify original and destination directories."; exit;
 elif [[ $# -gt 2 ]]; then
